8eca52b49dec19f7aabe50bea4123a1d0f5aa17b
aaronjc15128 <aaronjchauhan2@gmail.com>
2024-01-14 15:36:49 +0000

    Added fonts 'n' stuff

---
+++ lib/theme_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Map themeColors = {
+  "Text": const Color(0xFFFFFFFF),
+  "Background": const Color(0xFF141414),
+  "Primary": const Color(0xFFD89999),
+  "Secondary": const Color(0xFF782424),
+  "Accent": const Color(0xFF63C472),
+};
