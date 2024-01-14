2d716aa1b24ddc79c64ceee8658a36f066fe5bef
aaronjc15128 <aaronjchauhan2@gmail.com>
2024-01-14 19:02:09 +0000

    Added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -49,40 +49,183 @@ class _AppState extends State<App> {
       title: "fluttervine",
 
       home: Scaffold(
+        extendBodyBehindAppBar: true,
+        extendBody: true,
         backgroundColor: themeColors["Background"],
 
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(80),
+          child: AppBar(
+            elevation: 0,
+            actions: const [], // TODO: Add donate & github buttons
+            backgroundColor: Colors.transparent,
 
-          title: Text("FlutterVine", style: TextStyle(
-            fontFamily: "Poppins",
-            fontWeight: FontWeight.w300,
-            fontStyle: FontStyle.italic,
-            fontSize: 32,
-            color: themeColors["Text"],
-          )),
+            title: Text("FlutterVine", style: TextStyle(
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w300,
+              fontStyle: FontStyle.italic,
+              fontSize: 32,
+              color: themeColors["Text"],
+            )),
+          ),
         ),
 
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text("Streaming Service", textAlign: TextAlign.center, style: TextStyle(
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w600,
-              fontStyle: FontStyle.italic,
-              fontSize: 16,
-              color: themeColors["Text"]
-            )),
-            Text("Streaming Service", textAlign: TextAlign.center, style: TextStyle(
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w600,
-              fontStyle: FontStyle.italic,
-              fontSize: 16,
-              color: themeColors["Text"]
-            )),
-          ],
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [themeColors["Secondary"], themeColors["Background-Transparent"]],
+              begin: Alignment.topCenter,
+              end: Alignment.center,
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 17),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 50),
+                Text("by Aaron Chauhan", style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  color: themeColors["Text"],
+                )),
+                const SizedBox(height: 50),
+        
+                Text("Streaming Service", style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  color: themeColors["Text"]
+                )),
+                const SizedBox(height: 10),
+                Column(
+        
+                  children: [
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            fixedSize: const Size(130, 40),
+                            backgroundColor: themeColors["Box"],
+                          ),
+                          child: Text("BBC iPlayer", style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 16,
+                            color: themeColors["Text"]
+                          )),
+                        ), const SizedBox(width: 5),
+                        ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            fixedSize: const Size(130, 40),
+                            backgroundColor: themeColors["Box"],
+                          ),
+                          child: Text("ITVX", style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 16,
+                            color: themeColors["Text"]
+                          )),
+                        ), const SizedBox(width: 5),
+                        ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            fixedSize: const Size(130, 40),
+                            backgroundColor: themeColors["Box"],
+                          ),
+                          child: Text("All4", style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 16,
+                            color: themeColors["Text"]
+                          )),
+                        ), const SizedBox(width: 5),
+                        ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            fixedSize: const Size(130, 40),
+                            backgroundColor: themeColors["Box"],
+                          ),
+                          child: Text("My5", style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 16,
+                            color: themeColors["Text"]
+                          )),
+                        ), const SizedBox(width: 5),
+                      ],
+                    ), const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            fixedSize: const Size(130, 40),
+                            backgroundColor: themeColors["Box"],
+                          ),
+                          child: Text("STV Player", style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 16,
+                            color: themeColors["Text"]
+                          )),
+                        ), const SizedBox(width: 5),
+                        ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            fixedSize: const Size(130, 40),
+                            backgroundColor: themeColors["Box"],
+                          ),
+                          child: Text("UKTVPlay", style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 16,
+                            color: themeColors["Text"]
+                          )),
+                        ), const SizedBox(width: 5),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
+        
+                Text("Profile", style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  color: themeColors["Text"]
+                )),
+                const SizedBox(height: 50),
+        
+                Text("Download", style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  color: themeColors["Text"]
+                )),
+                const SizedBox(height: 50),
+              ],
+            ),
+          ),
         ),
 
       ),
--- lib/theme_colors.dart
+++ lib/theme_colors.dart
@@ -6,4 +6,7 @@ Map themeColors = {
   "Primary": const Color(0xFFD89999),
   "Secondary": const Color(0xFF782424),
   "Accent": const Color(0xFF63C472),
+
+  "Box": const Color(0x10FFFFFF),
+  "Background-Transparent": const Color(0x00141414),
 };
