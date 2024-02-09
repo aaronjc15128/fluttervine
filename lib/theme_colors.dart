6d07046d90ee5324af2768b127b9c4df500b1064
Aaron Chauhan <aaronjchauhan2@gmail.com>
2024-02-09 20:35:50 +0000

    Update themeColors.dart

--- lib/theme_colors.dart
+++ lib/theme_colors.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 Map themeColors = {
   "Text": const Color(0xFFFFFFFF),
   "Background": const Color(0xFF141414),
-  "Primary": const Color(0xFFD89999),
-  "Secondary": const Color(0xFF782424),
-  "Accent": const Color(0xFF63C472),
 
   "Box": const Color(0x10FFFFFF),
   "Background-Light": const Color(0xFF424242),
